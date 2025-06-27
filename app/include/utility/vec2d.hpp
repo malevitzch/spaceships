dbc3c59b17f12d7b579f1d3b0436544a1827f1f4
malevitzch <malevitzch@gmail.com>
2025-06-27 09:04:45 +0200

    Created an utility class for vectors that will be used inside the project

---
+++ app/include/utility/vec2d.hpp
@@ -0,0 +1,28 @@
+#ifndef VEC2D
+#define VEC2D
+
+#include <SFML/System/Vector2.hpp>
+
+namespace util {
+  struct Vec2d {
+    double x, y;
+
+    Vec2d(double x, double y);
+    Vec2d(sf::Vector2f vec);
+
+    Vec2d operator-() const;
+
+    Vec2d operator+(const Vec2d& vec) const;
+    Vec2d operator-(const Vec2d& vec) const;
+    Vec2d operator*(const double scalar) const;
+    Vec2d operator/(const double scalar) const;
+
+    operator sf::Vector2f() const; 
+
+    double magnitude() const;
+
+  };
+  Vec2d operator*(const double scalar, const Vec2d& vec);
+}
+
+#endif
---
+++ app/src/utility/vec2d.cpp
@@ -0,0 +1,38 @@
+#include "utility/vec2d.hpp"
+#include <cmath>
+
+namespace util {
+  Vec2d::Vec2d(double x, double y) : x(x), y(y) {}
+  Vec2d::Vec2d(sf::Vector2f vec) : x(vec.x), y(vec.y) {}
+
+  Vec2d Vec2d::operator-() const {
+    return (*this) * -1;
+  }
+
+  Vec2d Vec2d::operator+(const Vec2d& vec) const {
+    return Vec2d(this->x + vec.x, this->y + vec.y);
+  }
+  Vec2d Vec2d::operator-(const Vec2d& vec) const {
+    return Vec2d(this->x - vec.x, this->y - vec.y);
+  }
+  Vec2d Vec2d::operator*(const double scalar) const {
+    return Vec2d(this->x * scalar, this->y * scalar);
+  }
+  Vec2d Vec2d::operator/(const double scalar) const {
+    return Vec2d(this->x / scalar, this->y / scalar);
+  }
+
+  Vec2d::operator sf::Vector2f() const {
+    return sf::Vector2f(float(x), float(y));
+  }
+
+  double Vec2d::magnitude() const {
+    return std::sqrt(x*x + y*y);
+  }
+
+
+  Vec2d operator*(const double scalar, const Vec2d& vec) {
+    return Vec2d(vec.x * scalar, vec.y * scalar);
+  }
+
+}
