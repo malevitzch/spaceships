8179dd131448534f6ac8010133be508ee2f2df8a
malevitzch <malevitzch@gmail.com>
2025-07-06 15:05:24 +0200

    Added more accessors for physics object

--- app/include/physics/physics_object.hpp
+++ app/include/physics/physics_object.hpp
@@ -15,12 +15,18 @@ namespace physics {
     virtual void physicsTick(double dt) = 0;
 
     util::Vec2d getPosition() const;
+    double getPositionX() const;
+    double getPositionY() const;
     void setPosition(util::Vec2d target);
 
     util::Vec2d getVelocity() const;
+    double getVelocityX() const;
+    double getVelocityY() const;
     void setVelocity(util::Vec2d target);
 
     util::Vec2d getAcceleration() const;
+    double getAccelerationX() const;
+    double getAccelerationY() const;
     void setAcceleration(util::Vec2d target);
 
     util::Angle getAngle() const;
--- app/src/physics/physics_object.cpp
+++ app/src/physics/physics_object.cpp
@@ -6,6 +6,12 @@ namespace physics {
   util::Vec2d PhysicsObject::getPosition() const {
     return transform.position;
   }
+  double PhysicsObject::getPositionX() const {
+    return getPosition().x;
+  }
+  double PhysicsObject::getPositionY() const {
+    return getPosition().y;
+  }
   void PhysicsObject::setPosition(util::Vec2d target) {
     this->transform.position = target;
   }
@@ -13,12 +19,24 @@ namespace physics {
   util::Vec2d PhysicsObject::getVelocity() const {
     return transform.velocity;
   }
+  double PhysicsObject::getVelocityX() const {
+    return getVelocity().x;
+  }
+  double PhysicsObject::getVelocityY() const {
+    return getVelocity().y;
+  }
   void PhysicsObject::setVelocity(util::Vec2d velocity) {
     this->transform.velocity = velocity;
   }
 
   util::Vec2d PhysicsObject::getAcceleration() const {
     return transform.position;
+  }
+  double PhysicsObject::getAccelerationX() const {
+    return getAcceleration().x;
+  }
+  double PhysicsObject::getAccelerationY() const {
+    return getAcceleration().y;
   }
   void PhysicsObject::setAcceleration(util::Vec2d acceleration) {
     this->transform.acceleration = acceleration;
