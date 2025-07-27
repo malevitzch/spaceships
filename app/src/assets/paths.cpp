21d8e4e803308f9a6a77792ab33d45000723b74c
malevitzch <malevitzch@gmail.com>
2025-07-27 09:17:27 +0200

    Fix for windows

--- app/src/assets/paths.cpp
+++ app/src/assets/paths.cpp
@@ -4,9 +4,20 @@ namespace assets {
   namespace paths {
     std::string getAssetsPath() {
       namespace fs = std::filesystem;
-      static std::string assets_path =
-        fs::read_symlink("/proc/self/exe")
-          .parent_path().string() + "/../assets";
+      #ifdef _WIN32
+          wchar_t buffer[MAX_PATH];
+          DWORD len = GetModuleFileNameW(NULL, buffer, MAX_PATH);
+
+          std::filesystem::path exePath(buffer);
+          std::filesystem::path assets_path_win = exePath.parent_path() / ".." / "assets";
+
+          std::string assets_path = 
+            assets_path_win.lexically_normal().string();
+      #else
+        static std::string assets_path =
+          fs::read_symlink("/proc/self/exe")
+            .parent_path().string() + "/../assets";
+      #endif
       return assets_path;
     }
   }
